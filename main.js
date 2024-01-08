921ad25594ac124aab11f43b6a33535945a23aca
kfir <kfir.kotler@gmail.com>
2024-01-08 23:39:48 +0200

    basic

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+    document.getElementById("msg").classList.add("hidden")
+    document.getElementById("main").classList.remove("hidden")
+
+})
+
+function getUserInputString() {
+
+}
+
+function getUserInputNumber() {
+
+}
+
